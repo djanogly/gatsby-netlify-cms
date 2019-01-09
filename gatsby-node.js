6397815b00d6b168b1209a67ec26004da41a4738
Jack Djanogly <mrjackgd@gmail.com>
2019-01-09 22:27:45 +0000

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,13 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
   switch (stage) {
     case "build-html":
       actions.setWebpackConfig({
-        plugins: [webpack.DefinePlugin]
+        plugins: new webpack.DefinePlugin({
+          PRODUCTION: JSON.stringify(true),
+          VERSION: JSON.stringify("5fa3b9"),
+          BROWSER_SUPPORTS_HTML5: true,
+          TWO: "1+1",
+          "typeof window": JSON.stringify("object")
+        })
       })
     }
 };
