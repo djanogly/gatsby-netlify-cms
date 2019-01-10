4eb4c5fe333c0d88e4cbf67ad91fa93e12cf6fa4
Jack Djanogly <mrjackgd@gmail.com>
2019-01-10 17:27:22 +0000

    Update typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,6 @@
 import Typography from "typography"
 
 const typography = new Typography({
-  baseFontSize: "18px",
-  baseLineHeight: 1.666,
   headerFontFamily: [
     "Avenir Next",
     "Helvetica Neue",
